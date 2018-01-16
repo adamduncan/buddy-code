7acd2cda03e9522af9f9ad58018d7201abb6b4b8
Pete Robelou <probelou@gmail.com>
2018-01-16 18:39:26 +0000

    Webpack-dev-server and general updates.

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,30 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+// Plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/js/index.js', './src/styles/main.scss'],
-  output: {
-    filename: './build/js/bundle.js'
+  entry: './src/js/index.js',
+  devServer: {
+      contentBase: './dist',
+      port: 3000
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        exclude: /(node_modules|build)/,
-        loader: ExtractTextPlugin.extract(
-          Object.assign(
-            {
-              fallback: {
-                loader: require.resolve('style-loader')
-              },
-              use: [
-                {
-                  loader: require.resolve('css-loader'),
-                  options: {
-                  //   importLoaders: 1,
-                  //   minimize: true,
-                  //   sourceMap: false,
-                  },
-                },
-                {
-                  loader: require.resolve('sass-loader')
-                }
-              ],
-            }
-          )
-        ),
+        exclude: /(node_modules|dist)/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+        })
       },
       {
         test: /\.js$/,
-        exclude: /(node_modules|build)/,
+        exclude: /(node_modules|dist)/,
         use: {
           loader: 'babel-loader'
         }
@@ -44,9 +32,18 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('./build/css/[name].css'),
-    new CopyWebpackPlugin([
-      { context: 'src', from: '**/*.html', to: './build' }
-    ])
-  ]
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        title: 'Buddy Code',
+        template: './src/index.html'
+    }),
+    new ExtractTextPlugin({
+        filename: './dist/css/[name].css',
+        allChunks: true
+    }),
+  ],
+  output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
 };
